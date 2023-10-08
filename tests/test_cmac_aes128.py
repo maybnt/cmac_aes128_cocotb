d70ff57fbb8444052ee5e6d651bf88b9531dae69
czy_wml <zeyangchen@tju.edu.cn>
2023-10-09 00:21:49 +0800

    PulseDriver可以输入激励

--- tests/test_cmac_aes128.py
+++ tests/test_cmac_aes128.py
@@ -1,57 +1,55 @@
 import cocotb
+import random
 from cocotb.triggers import RisingEdge
 from cocotb.triggers import Timer
 from cocotb.clock import Clock
+from cocotb_bus.bus import Bus
+from cocotb_bus.drivers import BusDriver
 # from cocotb.regression import TestFactory 
 from cmac_aes128 import cmac_aes128
 
-print('*************************************begin**********************************')
+class PulseDriver(BusDriver):
+    _signals = [
+				'CLK',
+				'ld_Key',
+				'ld_Block',
+				'Done',
+				'Last_Block',
+				'KEY',
+				'TextIn',
+				'TextOut',
+				'Last_Block_Len'
+            ]
 
+    def __init__(self,dut):
+        super().__init__(dut,'',dut.CLK)
 
-
+    async def _driver_send(self,transaction,sync: bool = True) -> None:
+        if sync:
+            await RisingEdge(self.clock)
+        self.bus <= transaction 
 
 @cocotb.test()
 async def test_cmac_aes128(dut):
-    dut.CLK.value = 0
-    dut.Rst_n.value = 0
-    dut.ld_Block.value = 0
-    dut.ld_Key.value = 1
-    dut.Last_Block.value = 0
-    dut.Last_Block_Len.value = 0
-    dut.TextIn.value = 0
-    dut.KEY.value = 0x2b7e151628aed2a6abf7158809cf4f3c
-    print('***************************init done**********************************')
-    clock = Clock(dut.CLK,10,units="ns")
-    cocotb.start_soon(clock.start(start_high = False))
-    print('***************************clock running**********************************')
-    await Timer(100,units = 'ns')
-    dut.Rst_n.value = 1
-    await Timer(200,units = 'ns')
-    dut.ld_Key.value = 0
-    print('***************************reset done**********************************')
-    await Timer(500,units = 'ns')
-    dut.ld_Block.value = 1
-    dut.Last_Block.value = 1
-    dut.Last_Block_Len.value = 0
-    dut.TextIn.value = 0x6bc1bee22e409f96e93d7e117393172a
-    print('***************************value init**********************************')
-    await RisingEdge(dut.CLK)
-    dut.ld_Block.value = 0
-    dut.Last_Block.value = 0
-    dut.Last_Block_Len.value = 0
-    dut.TextIn.value = 0
-    print('***************************value init done**********************************')
-    await RisingEdge(dut.Done)
-    print(hex(dut.TextOut.value))
-    print('***************************&&&&&&&&&&&&&&&**********************************')
-# 定义一个十六进制数
-    hex_number = '6bc1bee22e409f96e93d7e117393172a'
-
-# 使用hex()函数将十六进制数转换为字符串
-    # string_number = hex(hex_number)
-    # string_number = str(string_number)
-    # string_number = string_number[2:]
-# hex()函数生成的字符串前缀为'0x'，所以需要将其去除
-    # print(string_number)
-    print(cmac_aes128(hex_number))
+    clock = Clock(dut.CLK,10,units='ns')
+    cocotb.start_soon(clock.start(start_high=False))
+    pulse_driver = PulseDriver(dut)
+    for i in range(10):
+        dut.ld_Key.value = 1
+        dut.ld_Block.value = 1
+        dut.Rst_n.value = 1
+        dut.Last_Block.value = 0
+        dut.KEY.value = random.randint(0,65535)
+        dut.TextIn.value = random.randint(0,65535)
+        dut.Last_Block_Len.value = 0
+        transaction = [
+                       dut.ld_Key,
+                       dut.ld_Block,
+                       dut.Last_Block,
+                       dut.KEY,
+                       dut.TextIn,
+                       dut.Last_Block_Len,
+                    ]
+        await pulse_driver.send(transaction)
+        
 
